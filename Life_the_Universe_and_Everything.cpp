a2a449d30deea6d42512ab238ec46748e46b68f9
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-08 16:26:39 +0530

    life the universe and everything added

---
+++ Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#define endl "\n"
+
+int main() {
+    while (1) {
+        long ele;
+        cin >> ele;
+        if (ele == 42)
+            break;
+        else {
+            cout << ele << endl;
+        }
+    }
+}
